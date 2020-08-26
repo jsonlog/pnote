c36610aa37422872d2758e8d191b7bc2f07e78d8
jsonlog <jsonlogit@gmail.com>
2020-08-26 19:51:29 +0800

    php artisan make:migration create_what_table

--- app/Product.php
+++ app/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //定义关联的表
-    protected $table = 'product2';
+    protected $table = 'product';
     
     protected $fillable = ['tradetime','type','tradeobject','goodsname','tag','money','tradestate','storeid','comment'];
     
