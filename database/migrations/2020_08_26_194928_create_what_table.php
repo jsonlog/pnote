8cfe3fa7152a5fea20e8738f795da2a603e56f5d
jsonlog <jsonlogit@gmail.com>
2020-08-26 20:16:05 +0800

    php artisan migrate

--- database/migrations/2020_08_26_194928_create_what_table.php
+++ database/migrations/2020_08_26_194928_create_what_table.php
@@ -14,11 +14,47 @@ class CreateWhatTable extends Migration
     public function up()
     {
         Schema::create('what', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table -> increments('id');
+            $table -> string('tag');
+            $table -> string('subtag')->nullable();
+            $table -> dateTime('now')->useCurrent();
+            $table -> dateTime('onwhen');
+            $table -> string('who');
+            $table -> string('inwhere');
+            $table -> string('forwhy')->nullable();
+            $table -> string('action')->nullable();
+            $table -> string('withwho')->nullable();
+            $table -> string('doing');
+            $table -> string('what');
+            $table -> integer('repet');
+            $table -> datetime('remind')->nullable();
+            $table -> integer('subwhat');
+            $table -> string('comment')->nullable();
+            $table -> string('source')->nullable();
+            $table -> string('url')->nullable();
+            $table -> tinyInteger('opened');
+            $table -> string('description')->nullable();
+            
+//             $table->id();
+//             $table->string('name');
+//             $table->string('email')->unique();
+//             $table->timestamp('email_verified_at')->nullable();
+//             $table->string('password');
+//             $table->rememberToken();
+//             $table->timestamps();
+            
+//             $table->string('email')->index();
+//             $table->string('token');
+//             $table->timestamp('created_at')->nullable();
+            
+//             $table->id();
+//             $table->text('connection');
+//             $table->text('queue');
+//             $table->longText('payload');
+//             $table->longText('exception');
+//             $table->timestamp('failed_at')->useCurrent();
         });
     }
-
     /**
      * Reverse the migrations.
      *
